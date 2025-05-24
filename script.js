3a15bbe61dbb4fd7df59fb3672a2327e11939bc8
Anuj Dubey <149352591+AnujDubey01@users.noreply.github.com>
2025-05-24 18:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+	 const listItems = document.querySelectorAll("ul li");
+	 const found = document.getElementById('level');
+	
+
+	
+listItems.forEach((item,index) => {
+	 if (item.id === "level") {
+   alert(`The level of the element is: ${index}`);
+  }
+});
